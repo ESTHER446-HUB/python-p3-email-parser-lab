9582a464b895378efb035398e50afea30e528568
ESTHER <esther.kuria@student.moringaschool.com>
2025-11-25 10:40:54 +0300

    parser lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+
+    def parse(self):
+        # TODO: implement parsing logic
+        return []
